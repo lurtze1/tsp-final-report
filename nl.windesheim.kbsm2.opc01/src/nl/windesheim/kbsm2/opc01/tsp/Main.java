ce0a2e727923aa3a5f0fd4dce3d2f45caccafe0e
lurtze1 <lurtze1@gmail.com>
2015-04-17 11:17:44 +0200

    Merge origin/master
    Conflicts:
    	nl.windesheim.kbsm2.opc01/src/nl/windesheim/kbsm2/opc01/tsp/MainScreen.java

---
+++ nl.windesheim.kbsm2.opc01/src/nl/windesheim/kbsm2/opc01/tsp/Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nl.windesheim.kbsm2.opc01.tsp;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Gebruiker
+ */
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        JFrame frame = new JFrame();
+        frame.setTitle("test");
+
+    }
+}
--- nl.windesheim.kbsm2.opc01/src/nl/windesheim/kbsm2/opc01/tsp/SimulationResults.java
+++ nl.windesheim.kbsm2.opc01/src/nl/windesheim/kbsm2/opc01/tsp/SimulationResults.java
@@ -5,6 +5,8 @@
  */
 package nl.windesheim.kbsm2.opc01.tsp;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.*;
 import javax.swing.*;
 
@@ -12,16 +14,29 @@ import javax.swing.*;
  *
  * @author Gebruiker
  */
-public class SimulationResults
-{        
-    private ArrayList<String> algorithmName;    
+public class SimulationResults extends JDialog implements ActionListener
+{
+
+    private ArrayList<String> algorithmName;
     private ArrayList<Integer> time;
     private ArrayList<Integer> squaresTraveled;
     private int fieldX;
     private int fieldY;
-    
+    private JLabel algoritme, tijd, vakjes, veld;
+
     public SimulationResults(JFrame frame)
     {
-        
+        super(frame, true);
+        setTitle("Options");
+        setSize(400, 800);
+        add(algoritme = new JLabel("algoritme"));
+        add(tijd = new JLabel("tijd"));
+        add(vakjes = new JLabel("vakjes"));
+        add(veld = new JLabel("veld"));
+    }
+
+    public void actionPerformed(ActionEvent e)
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
