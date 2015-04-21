069eae6d6d9793438d8117469496d9c8b761dd78
lurtze1 <lurtze1@gmail.com>
2015-04-21 11:34:52 +0200

    Merge origin/master
    Conflicts:
    	nl.windesheim.kbsm2.opc01/src/nl/windesheim/kbsm2/opc01/tsp/Options.java

---
+++ nl.windesheim.kbsm2.opc01/src/nl/windesheim/kbsm2/opc01/tsp/DistanceMap.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nl.windesheim.kbsm2.opc01.tsp;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Gebruiker
+ */
+public class DistanceMap
+{
+
+    public ArrayList<Packet> packets;
+    public double map[][];
+
+    public void createMatrix()
+    {
+        for (int i = 1; i < packets.size(); i++)
+        {
+
+        }
+    }
+}
